b2f5c6552de201c5ae63dc7c3847ecc58f7914b8
Garaeva Karina <garkaraeva@gmail.com>
2023-12-04 22:30:28 +0500

    changing style

--- View/VerticalMenu.py
+++ View/VerticalMenu.py
@@ -2,28 +2,21 @@ from tkinter import *
 
 class VerticalMenu:
     def __init__(this, title, parent,  height, width):
-<<<<<<< Updated upstream
-        this.frame = ttk.Frame(parent, height=height, width=width)
-        this.name_label = ttk.Label(this.frame, text=title)
-=======
         this.frame = Frame(parent, height=height, width=width, bg='#F1E9DB')
         this.frame.pack_propagate(0)
         this.name_label = Label(this.frame, text=title, bg='#F1E9DB')
->>>>>>> Stashed changes
         this.name_label.pack(side=TOP)
 
     def setButtons(this, buttonOptions):
-        pass
+        for buttonOption in buttonOptions:
+            button = buttonOption.getButton(this.frame)
+            button.pack(side=TOP, pady = 20)
 
     class ButtonOption:
         def __init__(this, title, command):
-            pass
+            this.title = title
+            this.command = command
 
-<<<<<<< Updated upstream
-        def getButton(this):
-            pass
-=======
         def getButton(this, parent):
-            button = Button(parent, text=this.title, command=this.command, height= 2, width=10, bg='#A39B8B')
+            button = Button(parent, text=this.title, command=this.command, bg='#A39B8B', width=30, height=3, fg='white')
             return button
->>>>>>> Stashed changes
