cd2b1b39231e897b0e29ab936fe7f6118ea3c0a4
Garaeva Karina <garkaraeva@gmail.com>
2023-12-04 13:14:42 +0500

    changing color of widgets

--- View/VerticalMenu.py
+++ View/VerticalMenu.py
@@ -1,10 +1,15 @@
 from tkinter import *
-from tkinter import ttk
 
 class VerticalMenu:
     def __init__(this, title, parent,  height, width):
+<<<<<<< Updated upstream
         this.frame = ttk.Frame(parent, height=height, width=width)
         this.name_label = ttk.Label(this.frame, text=title)
+=======
+        this.frame = Frame(parent, height=height, width=width, bg='#F1E9DB')
+        this.frame.pack_propagate(0)
+        this.name_label = Label(this.frame, text=title, bg='#F1E9DB')
+>>>>>>> Stashed changes
         this.name_label.pack(side=TOP)
 
     def setButtons(this, buttonOptions):
@@ -14,5 +19,11 @@ class VerticalMenu:
         def __init__(this, title, command):
             pass
 
+<<<<<<< Updated upstream
         def getButton(this):
             pass
+=======
+        def getButton(this, parent):
+            button = Button(parent, text=this.title, command=this.command, height= 2, width=10, bg='#A39B8B')
+            return button
+>>>>>>> Stashed changes
